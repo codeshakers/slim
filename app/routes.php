05d4e76b60f616435f0cfd7cc0ab9486b4fac9bd
Ramdhan Firmansyah <gedzsarjuncomuniti@gmail.com>
2020-04-24 19:00:25 +0800

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -4,9 +4,4 @@ use Slim\App;
 
 return function (App $app) {
     $app->get('/', \App\Action\HomeAction::class);
-    $app->get('/hajar-jahanam-super', \App\Action\HajarJahanamSuperAction::class);
-    $app->get('/lintah-papua', \App\Action\LintahPapuaAction::class);
-    $app->get('/lintah-kalimantan', \App\Action\LintahKalimantanAction::class);
-    $app->post('/pesan-barang', \App\Action\PesanBarangAction::class);
-    $app->get('/pesan-barang', \App\Action\PesanBarangAction::class);
 };
